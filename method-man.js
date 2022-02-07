a86c33bbee16a2349c130c5334842627a139c90e
Your Name <you@example.com>
2022-02-07 18:11:56 +0200

    circus

---
+++ method-man.js
@@ -0,0 +1,34 @@
+// Write 5 functions:
+
+// words that takes a string and split it into an array of strings on spaces
+// sentence that takes an array of strings and join them with spaces
+// yell that take a string and return it in upper case
+// whisper that take a string and return it in lower case and surround it with *
+// Create a capitalize function that takes a string and transforms it to upper 
+//case only for the first letter, and in lowercase for the rest of the string
+
+function words(a) {
+    let myArray = a.split(" ")
+    return myArray
+};
+
+function sentence(a) {
+    let myArray = a.join(' ')
+    return myArray
+};
+
+function yell(a) {
+    let myArray = a.toUpperCase()
+    return myArray
+};
+
+function whisper(a) {
+    let myArray = text.toLowerCase()
+    return myArray
+};
+
+function capitalize(a) {
+
+};
+
+console.log()
