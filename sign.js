cf4e0ecc88102f47ebc98bc44a13c64ca2aceb84
Your Name <you@example.com>
2022-02-07 19:33:54 +0200

    circus

---
+++ sign.js
@@ -0,0 +1,15 @@
+// Create the sign function that takes one number 
+// argument and return 1 if the number is positive, 
+// -1 if the number is negative and 0 
+// if the number is exactly 0 You must not just use Math.sign, make your own.
+
+// Create the sameSign function that takes 2 numbers as arguments and 
+// return true if they both have the same sign, or false otherwise.
+
+function sign(a){
+    return a===0 ? a : (a>0 ? 1 : -1) 
+}
+
+function sameSign(a){
+
+}
