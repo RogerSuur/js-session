b7724e24d255e6fe2fac50edd70ca3570953af05
Your Name <you@example.com>
2022-02-07 12:45:52 +0200

    last-first-kiss

---
+++ last-first-kiss.js
@@ -0,0 +1,19 @@
+// Create 3 functions:
+
+// a first function that takes an array or a string and return the first element.
+
+// a last function that takes an array or a string and return the last element.
+
+// a kiss function that returns an array of 2 elements the last and the first element, in that order
+
+function first (a) {
+    return a[0]
+};
+
+function last (a) {
+ 
+};
+
+function kiss (a) {
+
+};
