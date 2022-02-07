638d41792153604bb2a27a66cc7ef0e1ff4b2656
Your Name <you@example.com>
2022-02-07 17:33:13 +0200

    circus

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -3,6 +3,12 @@
 // smalls with the smallest possible number value
 // biggie with the greatest possible number value
 
-var smalls = (-1.7976931348623157e308 -1)
+var smalls = (-1.7976931348623157e308 -1);
 
-var biggie = (1.7976931348623157e308 + 1)
+
+var MAX_INT = Math.pow(2, 53); // 9 007 199 254 740 992
+for (var i = MAX_INT; i < MAX_INT + 2; ++i) {
+    biggie ++
+}
+
+var biggie = {}
