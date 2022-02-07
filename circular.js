78cc42e503942f9e64ea3a60d1ecaea7ae41023a
Your Name <you@example.com>
2022-02-07 17:02:17 +0200

    circus

--- circular.js
+++ circular.js
@@ -1,7 +1,7 @@
 // Create an object named circular that has a property named circular with itself as the value
 
 const circular= {
-    circular: function circular(a) {}
+    circular: () => circular
 };    
 
 
