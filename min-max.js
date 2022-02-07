cc834f1c6598fe5949ce1790774c220c774163ca
Your Name <you@example.com>
2022-02-07 19:29:09 +0200

    circus

---
+++ min-max.js
@@ -0,0 +1,15 @@
+// Create the max function that takes 2 number as arguments and returns the greatest
+
+// You must not just use Math.max, make your own.
+
+// Create the min function that takes 2 number as arguments and returns the lowest
+
+// You must not just use Math.min, make your own.
+
+function max(a,b){
+    return a>b ? a : b
+}
+
+function min(a,b){
+    return a<b ? a : b
+}
