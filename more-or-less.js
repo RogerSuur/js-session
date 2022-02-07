36cea037ebbe443aa5a7cf084ee07720ad9f2cbc
Your Name <you@example.com>
2022-02-07 12:26:27 +0200

    more-or-less

---
+++ more-or-less.js
@@ -0,0 +1,22 @@
+// Create 4 functions
+
+// more that takes 1 argument and add 1 to it
+// less that takes 1 argument and substract 1 to it
+// add that takes 2 arguments and add them
+// sub that takes 2 arguments and substract them
+
+function more (a) {
+    a + 1
+}
+
+function less(a) {
+    a - 1
+}
+
+function add(a, b) {
+    a  * b
+}
+
+function sub(a,b) {
+    a/b
+}
