d3d2e43945afb4256735f1a1933989a92305e307
Your Name <you@example.com>
2022-02-07 13:25:07 +0200

    concatstr

---
+++ change.js
@@ -0,0 +1,25 @@
+// Create 2 functions:
+
+// get: a function that takes a key and return the corresponding value from the sourceObject
+
+// set: a function that takes a key and a value update the value for the corresponding
+// property of the sourceObject and return the set value
+
+/*const sourceObject = {
+    num: 42,
+    bool: true,
+    str: 'some text',
+    log: console.log,
+}*/
+
+console.log(get('log'))
+
+function get (a) {
+    return sourceObject[a]
+};
+
+
+function set () {
+
+
+};
