997b86d5115d8409880ba83d4bb72b2ea3c32a87
Your Name <you@example.com>
2022-02-07 13:14:20 +0200

    concatstr

--- concat-str.js
+++ concat-str.js
@@ -1,9 +1,15 @@
 // Create a concatStr function that takes 2 arguments and concatenate them
 
+// t(() => concatStr('a', 'b') === 'ab')
+// t(() => concatStr('yolo', 'swag') === 'yoloswag')
+
+// handle non strings correctly
+// t(() => concatStr(1, 2) === '12')
+// t(() => concatStr(concatStr, concatStr) === String(concatStr).repeat(2))
 
 function concatStr (a,b) {
-    var texta = Number('a')
-    var textb = Number('b')
+    var texta = a.toString(a)
+    var textb = b.toString(b)
     var returnVariable = texta+textb;
     return returnVariable;
 };
