cddc40f1eb20b6d9aaac92572a2cdaea5dd2cef4
Your Name <you@example.com>
2022-02-07 12:42:14 +0200

    returns

---
+++ returns.js
@@ -0,0 +1,10 @@
+//id that takes one argument and return it
+//getLength that takes an array or a string and return its length
+
+function id (a) {
+    return a
+};
+
+function getLength (a) {
+  return a.length
+}
