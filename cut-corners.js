0349cfe357d4677b489e2b9f7a545b4864f5c351
Your Name <you@example.com>
2022-02-08 16:32:44 +0200

    tulebtuleb

---
+++ cut-corners.js
@@ -0,0 +1,29 @@
+// Create a function for each rounding math functions:
+
+// round (like Math.round)
+// ceil (like Math.ceil)
+// floor (like Math.floor)
+// trunc (like Math.trunc)
+// Some restrictions apply:
+
+// You may not use strings conversion to do it
+// No bitwise operator
+
+function round(input) {
+    for (let i = 0; i < input.length; i++) {
+        return i
+    }
+};
+
+function ceil() {
+
+};
+
+function floor() {
+
+};
+
+function trunc() {
+
+};
+
