97468dd7884e45c5ff8e09a77375703aa08d0e2d
Your Name <you@example.com>
2022-02-08 20:28:03 +0200

    tulebtuleb

--- unbreakable.js
+++ unbreakable.js
@@ -4,9 +4,14 @@
 
 // join that works like the string method .join but take the array as it's first argument.
 
-console.log(("first test:", split('a b c', ' '))); // ['a', 'b', 'c'])))
+//console.log(("first test:", split('a b c', ' '))); // ['a', 'b', 'c'])))
 //console.log(("secont test:", split('ggg - ddd - b', ' - '))); // ['ggg', 'ddd', 'b'])))
 //console.log("third test:", split('ee,ff,g,', ',')); // ['ee', 'ff', 'g', '']))
+
+
+console.log(join(['ee', 'ff', 'g', ''], ','))// === 'ee,ff,g,')
+console.log(join(['ggg', 'ddd', 'b'], ' - ')) //=== 'ggg - ddd - b')
+console.log(join(['a', 'b', 'c'], ' ')) //=== 'a b c')
 
 function split(inputArg, splitter) {
     var answer = [];
@@ -27,15 +32,25 @@ function split(inputArg, splitter) {
         } else {
             current += (inputArg[i])
         }
-        if (i===(inputArg.length-1) && inputArg[i] !== splitter){
-            console.log(current)
-            console.log(inputArg[i])
-            answer.push(inputArg[i])
+        if (i===(inputArg.length-1)){
+            //console.log(current)
+            //console.log(inputArg[i])
+            //answer.push(inputArg[i])
+            answer.push(current)
         }
     }
     return answer
 };
 
-function join(inputArg) {
-
+function join(inputArg, splitter) {
+    var answer = ''
+    inputArg.forEach((element, idx) => {
+        //console.log(element)
+        answer += element
+        //console.log(idx)
+        if (idx!==inputArg.length-1){
+        answer += splitter
+        }
+    });
+    return answer
 };
