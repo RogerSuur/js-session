d17586da04a70faa49e8eb7744c4ad5fb54fbfee
Your Name <you@example.com>
2022-02-08 15:47:25 +0200

    circus

---
+++ slicer.js
@@ -0,0 +1,41 @@
+// Create the slice function that works like Array.slice and String.slice
+
+// It takes an additional first argument which is the string or the array
+
+//console.log(slice('abcdef', 2)); //cdef
+//console.log(slice('abcdef', -2)); //ef
+//console.log(slice('abcdef', 0, 2)); //ab
+
+function slice(input, start = 0, end = 0) {
+    if (typeof input === "string") {
+       var answer = ""
+    } else {
+       var answer = []
+    }
+
+    var startAt = start
+    if (end === 0){
+        var endAt = input.length
+    } else {
+        var endAt = end
+    }
+    if (start < 0) {
+        startAt = (input.length + start)
+        endAt = input.length
+    }
+    if (end < 0) {
+        endAt = (input.length - end)
+    }
+
+    for (let i = startAt; i < (endAt) ; i++) {
+        if (typeof input === "string") {
+            console.log(i)
+            answer += input[i]
+        } else {
+            answer.push(input[i])
+        }
+    }
+    return answer
+    console.log(answer)
+};
+
