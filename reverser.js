7f71de5ffd67c5017015d3f7c202a00dc12ef55d
Your Name <you@example.com>
2022-02-08 18:12:56 +0200

    tulebtuleb

--- reverser.js
+++ reverser.js
@@ -5,16 +5,14 @@
 console.log(reverse([1, 2, 3]));
 
 function reverse(input) {
-
     if (typeof input === "string") {
         var answer = ""
     } else {
         var answer = []
     }
 
-    for (let i = 0; i <= input.length; i++) {
+    for (let i = 1; i <= input.length; i++) {
         if (typeof input === "string") {
-            console.log(i)
             answer += input[input.length-i]
         } else {
             answer.push(input[input.length-i])
