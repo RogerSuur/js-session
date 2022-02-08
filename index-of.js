315103cfb3fdc21a61132a9d211982fb2baffa50
Your Name <you@example.com>
2022-02-08 13:04:29 +0200

    circus

---
+++ index-of.js
@@ -0,0 +1,40 @@
+// Create 3 functions:
+
+// indexOf which returns the index of the first occurence of a value and
+//  takes as arguments an array to be searched, the value to be matched, and optionally the index from where to start searching from.
+// lastIndexOf which works just like the previous function 
+// but returns the index of the last occurence of a value
+// includes which returns true if the value was found in the array
+
+console.log(indexOf([1, 2, 3, 4, 5, 4, 3, 2, 1], 2));
+
+
+function indexOf(array, value, startIndex = 0) {
+    var found = ''
+    //for (let i = 0; i < array.length; i++) {
+    //    console.log(i, array[i])
+    //    if (array[i] === value) {
+    //        found = value
+    //    }
+    //}
+    let i = startIndex;
+    while ( i < array.length){
+        console.log(i,array[i])
+        i++;
+        if (array[i] === value) {
+            found = value
+            { break }
+        }
+    }
+    return found
+};
+
+function lastIndexOf() {
+
+};
+
+function includes() {
+
+};
+
+
