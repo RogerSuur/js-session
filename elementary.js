aa4bdf227d7ea534000546d2aa598c2a36d7e4bc
Your Name <you@example.com>
2022-02-08 12:03:17 +0200

    circus

--- elementary.js
+++ elementary.js
@@ -4,10 +4,10 @@
 // divide that do an integer division without using /
 // modulo that act like the % operator without using it
 
-console.log('positiivne', divide(78, 34));
-console.log('yks negatiivne', divide(-78, 34));
-console.log('teine negatiivne', divide(78, -34));
-console.log('mõlemad negatiivsed', divide(-78, -34));
+console.log('positiivne', modulo(34, 78));
+console.log('yks negatiivne', modulo(-78, 34));
+console.log('teine negatiivne', modulo(78, -34));
+console.log('mõlemad negatiivsed', modulo(-78, -34));
 
 function multiply(num1, num2) {
     if (num1 === 0) {
@@ -52,6 +52,24 @@ function divide(num1, num2) {
 
 };
 
-function modulo() {
-
+function modulo(num1, num2) {
+    var counter = Math.abs(num2);
+    var result = 0;
+    var posNum1 = Math.abs(num1)
+    while (posNum1 > counter) {
+        posNum1 = posNum1 -counter;
+        if (posNum1 < counter) {
+            result = posNum1
+        }
+    }
+    if (num1 < 0 && num2 < 0) {
+        return result
+    }
+    if (num1 < 0) {
+        return -result
+    }
+    if (num2 < 0) {
+        return -result
+    }
+    return result;
 };
