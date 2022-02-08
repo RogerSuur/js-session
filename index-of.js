9e6ca27982dbd9e167efa411c8a4a9847830f702
Your Name <you@example.com>
2022-02-08 13:40:36 +0200

    circus

--- index-of.js
+++ index-of.js
@@ -6,21 +6,16 @@
 // but returns the index of the last occurence of a value
 // includes which returns true if the value was found in the array
 
-//console.log(indexOf([1, 2, 3, 4, 5, 4, 3, 2, 1], 2));
-//console.log(indexOf([0, 0, t, t], t));
+//console.log(lastIndexOf([1, 2, 3, 4, 5, 4, 3, 2, 1], 2));
+//console.log(lastIndexOf([0, 0, 6, 6], 6, 3));
 //console.log(indexOf([5, 0, 0, 5], 5, 0));
 
 
 function indexOf(array, value, startIndex = 0) {
     var found = -1
-    //for (let i = 0; i < array.length; i++) {
-    //    console.log(i, array[i])
-    //    if (array[i] === value) {
-    //        found = value
-    //    }
-    //}
+
     let i = startIndex;
-    while ( i < array.length){
+    while (i < array.length) {
         if (array[i] === value) {
             //console.log(i, startIndex, array[i])
             found = i
@@ -31,7 +26,31 @@ function indexOf(array, value, startIndex = 0) {
     return found
 };
 
-function lastIndexOf() {
+function lastIndexOf(array, value, startIndex = 0) {
+    var found = -1
+
+    if (startIndex === 0) {
+        let i = array.length - 1;
+        while (i >= 0) {
+            if (array[i] === value) {
+                found = i
+                { break }
+            }
+            i--;
+        }
+        return found
+    }
+    if (startIndex !== 0) {
+        let i = startIndex;
+        while (i >= 0) {
+            if (array[i] === value) {
+                found = i
+                { break }
+            }
+            i--;
+        }
+        return found
+    }
 
 };
 
