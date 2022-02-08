55fc0722235017e5cac4e12c09b095b8058e1fb7
Your Name <you@example.com>
2022-02-08 15:53:02 +0200

    circus

---
+++ keep-cut.js
@@ -0,0 +1,15 @@
+// Create the cutFirst function that takes a string and remove the 2 first characters.
+
+// Create the cutLast function that takes a string and remove the 2 last characters.
+
+// Create the cutFirstLast function that takes a string as parameter and remove the 2 first characters and 2 last characters.
+
+// Create a keepFirst function that takes a string as parameter and return the string only keeping the 2 first characters.
+
+// Create a keepLast function that takes a string as parameter and return the string only keeping the 2 last characters.
+
+// Create a keepFirstLast function that takes a string as parameter and only keep 2 first characters and 2 last characters.
+
+function cutFirst(input){
+    return str = input.substring(1)
+}
