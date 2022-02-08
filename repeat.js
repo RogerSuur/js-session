98570e3b6de470bc0d0d7b899fc9841ecc21319b
Your Name <you@example.com>
2022-02-08 11:04:40 +0200

    circus

---
+++ repeat.js
@@ -0,0 +1,15 @@
+// Create a repeat function that takes a string and a number as parameters
+//  and return the repeated string by the given number 
+//  Like the method developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+//  Of course you may not use the method directly
+
+//console.log(repeat('ha',3));
+
+function repeat(a,b) {
+    var result = [];
+    for (let index = 1; index <= b; index++) {
+        //console.log(index)
+        result.push(a);
+    }
+    //console.log(result)
+};
