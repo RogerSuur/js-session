6833978a74ec5106efb2921d3f1a40197c61dd86
Your Name <you@example.com>
2022-02-08 17:42:47 +0200

    tulebtuleb

--- cut-corners.js
+++ cut-corners.js
@@ -9,31 +9,42 @@
 // You may not use strings conversion to do it
 // No bitwise operator
 
-//console.log(round(3.2442352314))
+//console.log(round(-3.2442352314))
 //console.log("trunc:", trunc(42.84)) //42
 //console.log("trunc", trunc(-0.123)) // -0
 //console.log("floor:", floor(42.84)) //42
 //console.log("floor:", floor(5.05)) //5
 //console.log("floor:", floor(-5.05)) //-6
-console.log("ceil", ceil(.95)); // 1
-console.log("ceil", ceil(4)); // 4
-console.log("ceil", ceil(7.004)); // 8
-console.log("ceil", ceil(-0.95)); // -0
-console.log("ceil", ceil(-4)); // -4
-console.log("ceil", ceil(-7.004)); // -7
+//console.log("ceil", ceil(.95)); // 1
+//console.log("ceil", ceil(4)); // 4
+//console.log("ceil", ceil(7.004)); // 8
+//console.log("ceil", ceil(-0.95)); // -0
+//console.log("ceil", ceil(-4)); // -4
+//console.log("ceil", ceil(-7.004)); // -7
 
 function round(num) {
     var remainder = num % 1
-    if (remainder > 0.5) {
-    return remainder +1
+    console.log(remainder)
+    if(num > 0 ){
+        if (remainder > 0.5) {
+            return (trunc(num) + 1)
+        } else {
+            return trunc(num)
+        }
+    } else {
+        if (remainder < -0.5) {
+            return (trunc(num) - 1)
+        } else {
+            return trunc(num)
+        } 
     }
 };
 
 function ceil(num) {
     if (num % 1 !== 0) {
-        return trunc(num) +1
+        return trunc(num) + 1
     } else {
-        num - num % 1 -1
+        num - num % 1 - 1
     }
 };
 
