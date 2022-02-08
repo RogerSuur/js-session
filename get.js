c5ffd716199486ad66a7bf3315f623077de8f398
Your Name <you@example.com>
2022-02-09 00:02:45 +0200

    tulebtuleb

---
+++ get.js
@@ -0,0 +1,14 @@
+// Create the get function. It takes 2 arguments:
+
+// src an object
+// path a string
+// And returns the value at the given string path.
+
+// Example:
+// const src = { nested: { key: 'peekaboo' } }
+// const path = 'nested.key'
+// get(src, path) // -> 'peekaboo'
+
+function get(src, path){
+
+};
