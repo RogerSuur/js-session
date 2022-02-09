0d89379c252838852e46b3508cf08b9c97633194
Your Name <you@example.com>
2022-02-09 15:12:59 +0200

    tulebtuleb

--- letter-space-number.js
+++ letter-space-number.js
@@ -2,20 +2,21 @@
 // followed by a space, followed by a number only if that number has only one digit and is not followed by any letter.
 
 
-console.log(letterSpaceNumber('I like 7up.')); // []))
-console.log(letterSpaceNumber("It's 20 past 3")); // ['t 3']))
-console.log(letterSpaceNumber('example 1, example 2')); // ['e 1', 'e 2']))
-console.log(letterSpaceNumber(''));// []))
-console.log(letterSpaceNumber('Definitely 9.'));// ['y 9']))
+//console.log(letterSpaceNumber('I like 7up.')); // []))
+//console.log(letterSpaceNumber("It's 20 past 3")); // ['t 3']))
+//console.log(letterSpaceNumber('example 1, example 2')); // ['e 1', 'e 2']))
+//console.log(letterSpaceNumber(''));// []))\
+//console.log(letterSpaceNumber('Definitely 9.'));// ['y 9']))
 
 function letterSpaceNumber(inputString) {
 
     const str = inputString;
-    const re = /\w\s\d\W/g;
-    const found = str.match(re);
-    console.log(found)
+    var re = /\w\s\d\b/g;
+    var found = str.match(re);
+    //console.log(found)
 
     for (let i = 0; i < str.length; i++) {
-        const element = array[i];
+        const element = str[i];
     }
+    return found
 };
