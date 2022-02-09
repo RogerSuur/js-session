e1e892eb2319adbdf44249060b3995e897e39db3
Your Name <you@example.com>
2022-02-09 20:04:26 +0200

    tulebtuleb

--- ion-out.js
+++ ion-out.js
@@ -1,12 +1,22 @@
 // Make a function ionOut that receives a string and returns an array with every word containing 'ion' following a t, without the 'ion'.
-console.log(ionOut('attention, direction')); //, ['attent', 'direct']))
-console.log(ionOut('promotion, provision')); //, ['promot']))
-console.log(ionOut('transfusion')); //, []))
+//console.log(ionOut('attention, direction')); //, ['attent', 'direct']))
+//console.log(ionOut('promotion, provision')); //, ['promot']))
+//console.log(ionOut('transfusion')); //, []))
 
 
 function ionOut(inputString) {
     var str = inputString
-    var re = /(tion)/g;
-    var found = str.replace(re, '');
-    return found
+    var fullWord = /(\w+)(?:tion)/g;
+    var cuttedPart = /(?:tion)/g
+    var found = str.match(fullWord);
+    //console.log(found)
+   var result = []
+    if (found === null) {
+        return result
+    }
+    for (let i = 0; i < found.length; i++) {
+        //console.log(found[i])
+        result.push(found[i].replace(cuttedPart, 't'))
+    }
+    return result
 };
