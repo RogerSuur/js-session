52f33662186c4cb1005f4c60f576bff66da351b2
Your Name <you@example.com>
2022-02-09 13:18:50 +0200

    tulebtuleb

--- pyramid.js
+++ pyramid.js
@@ -18,34 +18,17 @@
 //console.log(pyramid('#', 40));
 
 function pyramid(string, depth) {
-    let n = depth;
-    let answer = "";
-    for (let i = 1; i <= n; i++) {
-        for (let j = 1; j <= n - i; j++) {
-            answer += " ";
-        }
-        for (let k = 0; k < 2 * i - 1; k++) {
-            answer += string;
-        }
-        if( i !== n) {
-            answer += "\n"}
-    }
+   let n = depth;
+   let answer = "";
+   for (let i = 1; i <= n; i++) {
+       for (let j = 1; j <= n - i; j++) {
+           answer += " ".repeat(string.length);
+       }
+       for (let k = 0; k < 2 * i - 1; k++) {
+           answer += string;
+       }
+       if( i !== n) {
+           answer += "\n"}
+   }
 return answer
 };
-
-
-//function pyramid(str, int) {
-//    let res = ''
-//    let currentLine = 1
-//    for (let i = 1; i <= int; i++) {
-//        res += ' '.repeat(int - i).repeat(str.length)
-//        res += str.repeat(i * 2 - 1)
-//        if (i === int) {
-//            return res
-//        }
-//        res += '\n'
-//    }
-//
-//    return res
-//}
-//console.log(pyramid('{}', 12))
