33f77bf79cdd207d064b6be623eeb73cfefbe0f8
Your Name <you@example.com>
2022-02-09 11:39:57 +0200

    tulebtuleb

--- triangle.js
+++ triangle.js
@@ -1,4 +1,7 @@
-// Create a triangle function that takes a string and a number as parameters and return a triangle constructed by the string passed as argument and with the depth of the number passed as argument.
+// Create a triangle function that takes a string and a 
+//number as parameters and return a triangle constructed
+//by the string passed as argument and with the depth of the 
+//number passed as argument.
 
 // Output example
 // * character and depth of 5:
@@ -8,3 +11,14 @@
 // ****
 // *****
 // No new line in last line
+//(triangle('#', 4));
+//(triangle('a', 5));
+//(triangle('+', 10));
+
+function triangle(string, depth) {
+    var counter = ''
+    for (let i = 0; i < depth; i++) {
+        counter += string
+        console.log(counter)
+    }
+};
