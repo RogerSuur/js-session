d28de719249a85ad17f496612e45012b3c2df173
Your Name <you@example.com>
2022-02-09 11:16:32 +0200

    tulebtuleb

--- get.js
+++ get.js
@@ -9,6 +9,28 @@
 // const path = 'nested.key'
 // get(src, path) // -> 'peekaboo'
 
-function get(src, path){
+//Tests
+// work with simple key / values
+//console.log(get({ key: 'value' }, 'key')); // === 'value')
+// work with nested objects
+//console.log(get({ nested: { key: 'value' }})); //'nested.key') === 'value')
+// return undefined without error if the value do not exist
+//console.log(get({ key: 'value' }, 'nx')); //=== undefined)
+//console.log(get({ nested: { key: 'value' } })); //, 'nested.nx') === undefined)
+//console.log(get({ nested: { key: 'value' } })); // 'nx.nx') === undefined)
+// work with nested arrays too
+//console.log(get({ a: [{ b: t }] }, 'a.0.b'));// === t)
+//console.log(get({ a: [{ b: t }] }, 'a.0.b.toString')); //=== t.toString
 
+function get(src, path){    
+   let pathArr = path.split('.')
+   
+   pathArr.forEach(e => {
+       if (src === 'undefined'){
+           return src
+       }
+       return src[e]
+    });
+
+   return src
 };
