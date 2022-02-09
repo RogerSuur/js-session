a63cd1f0ce1f0f060e96e02018681630adf19266
Your Name <you@example.com>
2022-02-09 14:14:11 +0200

    tulebtuleb

---
+++ its-a-match.js
@@ -0,0 +1,18 @@
+// Create 4 regular expression in variables:
+
+// normal matches with the expression 'hi'.
+
+// begin matches with the expression 'hi', only when it is in the beginning.
+
+// end matches with the expression 'hi', only when it is in the end.
+
+// beginEnd matches with the expression 'hi', only when it is exactly hi.
+
+
+var normal = 'hi'.match(regex);
+
+var begin = 'hi'.match(regex);
+
+var end ='hi'.match(regex);
+
+var beginEnd ='hi'.match(regex);
