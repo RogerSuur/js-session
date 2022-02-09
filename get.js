2c9026219f8730084aff4bf07b3e756c28747138
Your Name <you@example.com>
2022-02-09 11:29:46 +0200

    tulebtuleb

--- get.js
+++ get.js
@@ -12,9 +12,9 @@
 //Tests
 // work with simple key / values
 //console.log(get({ key: 'value' }, 'key')); // === 'value')
-// work with nested objects
+//// work with nested objects
 //console.log(get({ nested: { key: 'value' }})); //'nested.key') === 'value')
-// return undefined without error if the value do not exist
+//// return undefined without error if the value do not exist
 //console.log(get({ key: 'value' }, 'nx')); //=== undefined)
 //console.log(get({ nested: { key: 'value' } })); //, 'nested.nx') === undefined)
 //console.log(get({ nested: { key: 'value' } })); // 'nx.nx') === undefined)
@@ -22,15 +22,13 @@
 //console.log(get({ a: [{ b: t }] }, 'a.0.b'));// === t)
 //console.log(get({ a: [{ b: t }] }, 'a.0.b.toString')); //=== t.toString
 
-function get(src, path){    
+function get(obj, path){    
    let pathArr = path.split('.')
+   let val = obj
    
-   pathArr.forEach(e => {
-       if (src === 'undefined'){
-           return src
-       }
-       src[e]
+   pathArr.forEach((e) => {
+       if (val === undefined) return val  
+           val = val[e]
     });
-
-   return src
+   return val
 };
