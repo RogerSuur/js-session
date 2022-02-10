56e6e5820beb223fd4b3e035eb9f3b2c4003e9d1
Your Name <you@example.com>
2022-02-10 17:02:31 +0200

    tulebtuleb

--- date-is.js
+++ date-is.js
@@ -16,7 +16,7 @@
 //console.log(isValid(new Date('1995-12-17T03:24:00')))
 
 function isValid(input){
-    if ((!isNaN(new Date(input))) && (new Date(input))) {
+    if ((!isNaN(input)) && (input)) {
         return true
     } else {
         return false
