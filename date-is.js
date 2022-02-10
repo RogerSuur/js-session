76bef1978a50c19ce15464ac8f9929600134c856
Your Name <you@example.com>
2022-02-10 16:56:35 +0200

    tulebtuleb

--- date-is.js
+++ date-is.js
@@ -16,7 +16,11 @@
 //console.log(isValid(new Date('1995-12-17T03:24:00')))
 
 function isValid(input){
-    return (!isNaN(new Date(input))) && (new Date(input) !== 0) 
+    if ((!isNaN(new Date(input))) && (new Date(input) !== 0)) {
+        return true
+    } else {
+        return false
+    }
 };
 
 function isAfter(){
