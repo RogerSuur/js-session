675785c49a5239ee45ed4527a4b4d421a0cb8b26
Your Name <you@example.com>
2022-02-11 21:02:49 +0200

    tulebtuleb

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,6 +1,6 @@
 //Make a dayOfTheYear functions that takes a date and returns the number of days since the first day of that year
 
-//console.log(dayOfTheYear(new Date('0001-01-01'))); //=== 1)
+console.log(dayOfTheYear(new Date('0001-01-01'))); //=== 1)
 console.log(dayOfTheYear(new Date('1664-08-09'))); //=== 222)
 console.log(dayOfTheYear(new Date('1600-12-31'))); //=== 366)
 console.log(dayOfTheYear(new Date('2020-06-22'))); //=== 174)
@@ -24,5 +24,5 @@ function dayOfTheYear(inputDate){
    // let wantedDate = new Date(inputDate)
 //
    daysFrom = (inputDate - startingYear) / (1000 * 60 * 60 * 24)
-    return daysFrom+2
+    return Math.trunc(daysFrom+2)
 };
