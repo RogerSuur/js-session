47d3080398e97508ffc891d1569266010a35df13
Your Name <you@example.com>
2022-02-11 19:35:48 +0200

    tulebtuleb

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,6 +1,6 @@
 // Make a countLeapYears functions that takes a date and returns the number of leap years since year 1
 
-//console.log(countLeapYears(new Date('0001-12-01'))) // === 0)
+//console.log(countLeapYears(new Date('0012-12-01'))) // === 0)
 //console.log(countLeapYears(new Date('1664-08-09'))) // === 403)
 //console.log(countLeapYears(new Date('2020-01-01'))) // === 489)
 //console.log(countLeapYears(new Date('2048-12-08'))) // === 496)
@@ -9,24 +9,11 @@
 function countLeapYears(inputDate) {
 
     var myYear = inputDate.getFullYear()
-    console.log("myYear:", myYear);
     var answer = 0
     for (let i = 1; i <= myYear; i++) {
-        if (myYear % 4 == 0) {
+        if ((i % 4) === 0) {
             answer ++
         } 
     }
     return answer
-    
-
 }
-
-// function isLeapYear(year) {
-//     if (year % 4 == 0) {
-//         console.log("leap year")
-//     } else {
-//         console.log("Not a leap year")
-//     }
-// }
-// var myYear = 2020;
-// isLeapYear(myYear)
