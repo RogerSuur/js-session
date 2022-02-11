6ad699414902f9c48833f4d5cfdbcf161ac3e7ed
Your Name <you@example.com>
2022-02-11 19:49:00 +0200

    tulebtuleb

--- count-leap-years.js
+++ count-leap-years.js
@@ -5,15 +5,21 @@
 //console.log(countLeapYears(new Date('2020-01-01'))) // === 489)
 //console.log(countLeapYears(new Date('2048-12-08'))) // === 496)
 
-
 function countLeapYears(inputDate) {
-
     var myYear = inputDate.getFullYear()
+    console.log("myYear:", myYear);
     var answer = 0
-    for (let i = 1; i <= myYear; i++) {
-        if ((i % 4) === 0) {
+    for (let i = 5; i <= myYear; i++) {
+        if (isLeapYear(i)) {
+            console.log("added answer++", i ,answer)
             answer ++
         } 
     }
     return answer
 }
+
+function isLeapYear(year) {
+    if (year % 400 === 0) return true;
+    if (year % 100 === 0) return false;
+    return year % 4 === 0;
+  }
