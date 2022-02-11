fb9e6f35d7d1dda36b595590cf70dfd351fb8fa4
Your Name <you@example.com>
2022-02-11 12:03:12 +0200

    tulebtuleb

---
+++ rebecca-black.js
@@ -0,0 +1,57 @@
+// Instructions
+// Create the following functions:
+
+// isFriday returns true if the date is a friday
+// isWeekend returns true if the date is a day of the weekend
+// isLeapYear returns true if the year is a leap year
+// isLastDayOfMonth returns true if the date is the last day of the month
+
+// isFriday
+//console.log( isFriday(new Date('2014-08-29')));
+//console.log( isFriday(new Date('2020-07-17')));
+//console.log( !isFriday(new Date('1992-08-26')));
+//console.log( !isFriday(new Date('2000-08-26')));
+
+// isWeekend
+console.log(isWeekend(new Date('2014-09-06')));
+console.log(isWeekend(new Date('2020-05-30')));
+console.log(!isWeekend(new Date('1929-02-13')));
+console.log(!isWeekend(new Date('1990-01-30')));
+
+// isLeapYear
+//console.log(isLeapYear(new Date('1804-02-01')));
+//console.log(isLeapYear(new Date('2008-02-01')));
+//console.log(isLeapYear(new Date('2216-02-01')));
+//console.log(!isLeapYear(new Date('1993-02-01')));
+//console.log(!isLeapYear(new Date('1999-02-01')));
+
+// isLastDayOfMonth
+//console.log(isLastDayOfMonth(new Date('2020-02-29')));
+//console.log(isLastDayOfMonth(new Date('2020-12-31')));
+//console.log(isLastDayOfMonth(new Date('2019-02-28')));
+//console.log(isLastDayOfMonth(new Date('1998-02-28')));
+//console.log(isLastDayOfMonth(new Date('1980-02-29')));
+//console.log(!isLastDayOfMonth(new Date('2020-12-30')));
+//console.log(!isLastDayOfMonth(new Date('2020-02-28')));
+//console.log(!isLastDayOfMonth(new Date('2019-02-29')));
+
+function isFriday(date){
+    let day = date.getDay();
+    if (day !== false)
+    return true
+
+};
+
+function isWeekend(date){
+    let day = date.getDay();
+    if (day !== false)
+    return true
+};
+
+function isLeapYear(date){
+
+};
+
+function isLastDayOfMonth(date){
+
+};
