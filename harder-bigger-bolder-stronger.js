75818c1764f2308809f7bb5912300cc679505c14
Your Name <you@example.com>
2022-02-13 15:30:41 +0200

    tulebtuleb

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -7,15 +7,17 @@ export function generateLetters(){
           text += possible.charAt(Math.floor(Math.random() * possible.length));
         //document.getElementById('div').innerHTML = text;
             element.append(text)
-            //document.getElementById('div').style.fontSize = (i+1)+"px";
             element.style.fontSize= i+10+'px';
             
             // font-weight has to be 300 for the first third of the letters, 400 for the second third, and 600 for the last third
             if(i<40){
+                console.log(i, "font: 300")
                 element.style.fontWeight = '300';
-            } else if (80>i>=40) {
+            } else if (40<=i && i<80) {
+                console.log(i, "font: 400")
                 element.style.fontWeight = '400';
             } else if ( i>=80) {
+                console.log(i, "font: 600")
                 element.style.fontWeight = '600';
             }
         }
