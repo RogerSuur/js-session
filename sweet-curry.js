c022c8d6cbb52747a833927b20017052f8e2329b
Your Name <you@example.com>
2022-02-18 22:17:07 +0200

    style

--- sweet-curry.js
+++ sweet-curry.js
@@ -3,3 +3,19 @@ function mult2(arg){
         return arg*b
     }
 }
+function add3(arg){
+    return function(b){
+        return function(c){
+            return arg+b+c
+        }
+    }
+}
+function sub4(arg){
+    return function(b){
+        return function(c){
+            return function(d){
+                return arg-b-c-d
+            }
+        }
+    }
+}
