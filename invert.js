586e65150ec0d5b95b94f33eeeccb11547a90850
Your Name <you@example.com>
2022-02-19 19:37:27 +0200

    banger

---
+++ invert.js
@@ -0,0 +1,16 @@
+// Instructions
+// Create a function called invert that takes an object and returns it with its keys and values inverted.
+
+function invert (obj){
+    var retobj = {};
+  for(var key in obj){
+    if (obj.hasOwnProperty(key)) {   
+    retobj[obj[key]] = key;
+    }
+  }
+  return retobj;
+
+    
+    
+
+}
